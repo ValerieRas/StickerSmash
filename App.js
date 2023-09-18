2df0d9b15e9b3c8d257358f2156505b36a132ee3
valerieRas <rasolovalerie@gmail.com>
2023-09-18 16:30:07 +0200

    added emojis and double tap

--- App.js
+++ App.js
@@ -5,14 +5,37 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import { useState, useref } from 'react';
+
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
+
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef} from 'react-native-view-shot';
+
 
 const PlaceHolderImage = require('./assets/images/background-image.png');
 
 
 export default function App() {
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
   const [selectedImage, setSelectedImage] = useState(null);
+
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  const imageRef = useRef();
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -24,29 +47,79 @@ export default function App() {
 
       setSelectedImage(result.assets[0].uri);
 
+      setShowAppOptions(true);
+
     } else {
       alert('You did not select any image.');
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  if (status === null) {
+    requestPermission();
+  }
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
 
       <View style={styles.imageContainer}>
+      <View ref={imageRef} collapsable={false}>
         <ImageViewer
           placeholderImageSource={PlaceHolderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
       </View>
 
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
 
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -69,4 +142,13 @@ const styles = StyleSheet.create({
     flex: 1 / 3,
     alignItems: 'center',
   },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+
 });
